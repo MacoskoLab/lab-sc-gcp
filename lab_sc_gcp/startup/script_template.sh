7351f57929a332fec729287a5603e794a52656c4
vkozareva <velina.kozareva@gmail.com>
2020-07-08 11:12:22 -0400

    Fix startup script

--- lab_sc_gcp/startup/script_template.sh
+++ lab_sc_gcp/startup/script_template.sh
@@ -2,11 +2,10 @@
 
 # Add user if necessary
 id '${USER}' >/dev/null 2>&1
-if [ \$? -eq 1 ] ; then
+if [ $? -eq 1 ] ; then
    adduser --disabled-password --gecos '' '${USER}'
 fi
 # Set password for user (for rstudio)
 echo '${USER}:${R_PASS}' | chpasswd
 usermod -a -G staff '${USER}'
-mkdir -p /home/data/libraries && chmod 777 /home/data && chmod 777 /home/data/libraries/
-mkdir -p /home/downloads && chmod 777 /home/downloads
+usermod -a -G google-sudoers '${USER}'
